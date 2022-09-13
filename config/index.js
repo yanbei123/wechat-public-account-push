44db39fdc17386064f2a59e2a4ba8ce6b31c7102
燕北 <60458625+yanbei123@users.noreply.github.com>
2022-09-13 11:20:53 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -38,13 +38,13 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oR-x-57lS8_MOyHwSmJbKkgZHfEE",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "tLQ-tQFgi-JjQqQjAW32cH4PVGu6jjVz_ivf9VrHuKo",
+      useTemplateId: "8I5AJg_XAv3Z1l6npcZNkCMkUQsosdqe_7XFF_LzFVg",
       // 所在省份
       province: "安徽",
       // 所在城市
       city: "马鞍山",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://www.baidu.com"
+      openUrl: ""
     },/*,
     {
       name: "老婆1", 
